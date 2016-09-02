173391bfbff72906ced70feb950c7301b82b5625
superdroid <superdroid@126.com>
2016-09-02 11:30:10 +0800

    Initial commit

---
+++ app/src/main/java/tao/test/weixindemo/view/AudioRecoderButton.java
@@ -0,0 +1,20 @@
+package tao.test.weixindemo.view;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.Button;
+
+/**
+ * Created by lisongtao on 2016/9/2 09:27.
+ * desc：自定义按钮
+ */
+public class AudioRecoderButton extends Button {
+    public AudioRecoderButton(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public AudioRecoderButton(Context context) {
+        this(context, null);
+    }
+
+}
