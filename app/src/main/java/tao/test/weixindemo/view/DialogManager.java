e29d26ed68ed765bb064e4649002b4c1a0fa24e8
superdroid <superdroid@126.com>
2016-09-27 15:57:14 +0800

    dialog布局完成

---
+++ app/src/main/java/tao/test/weixindemo/view/DialogManager.java
@@ -0,0 +1,12 @@
+package tao.test.weixindemo.view;
+
+import android.app.Dialog;
+
+/**
+ * Created by lisongtao on 2016/9/27 15:44.
+ * desc：Dialog的管理者
+ */
+public class DialogManager {
+
+    private Dialog mDialog;
+}
